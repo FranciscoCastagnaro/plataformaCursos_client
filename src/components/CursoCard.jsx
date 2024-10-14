8d8d37e6bb1a410923f7efa03f982f373937b43e
FranciscoCastagnaro <francastagnaro@hotmail.com>
2024-10-14 18:09:20 -0300

    dates formating

--- src/components/CursoCard.jsx
+++ src/components/CursoCard.jsx
@@ -1,4 +1,4 @@
-import './styles/cursocard.css'
+import "./styles/cursocard.css";
 
 export const CursoCard = ({
   descripcion,
@@ -8,12 +8,20 @@ export const CursoCard = ({
   fechaInicio,
   profesor,
 }) => {
+
+  function transformarFecha(fecha) {
+    const year = fecha.substring(0, 4);
+    const month = fecha.substring(4, 6);
+    const day = fecha.substring(6, 8);
+    return `${day}/${month}/${year}`;
+  }
+
   return (
     <div className="curso-card">
       <h3 className="curso-titulo">{descripcion}</h3>
       <p className="curso-categoria">Categoría: {categoria}</p>
       <p className="curso-profesor">Instructor: {profesor}</p>
-      <p className="curso-fecha">Fecha de inicio: {fechaInicio}</p>
+      <p className="curso-fecha">Fecha de inicio: {transformarFecha(fechaInicio)}</p>
       <div className="curso-vacantes">
         <p>
           Vacantes: {vacantesDisponibles}/{vacantesTotales}
